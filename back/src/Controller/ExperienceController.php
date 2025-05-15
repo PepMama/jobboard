4485cb4a96c47934e3e1a06088a6a8c3ae998953
Maelic Pepin <pmaelic@outlook.fr>
2025-05-15 22:29:01 +0200

    PM - CRUD experience

---
+++ back/src/Controller/ExperienceController.php
@@ -0,0 +1,112 @@
+<?php 
+
+namespace App\Controller;
+
+use App\Service\TokenService;
+use App\Service\ExperienceService;
+use App\Entity\Student;
+use App\Entity\Users;
+use App\Entity\Experience;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class ExperienceController extends AbstractController
+{
+    public function modelJson(array $experiences): array
+    {
+        return array_map([$this, 'formatExperienceJson'], $experiences);
+    }
+
+    #[Route('/student/manage-experience', name: 'app_manage_experience', methods: ['POST'])]
+    public function manageExperience(Request $request, ExperienceService $experienceService, TokenService $tokenService): JsonResponse
+    {
+        try {
+            $user = $tokenService->getUserFromRequest($request);
+            if($user->getRole() !== 'student'){
+                return new JsonResponse(['error' => 'Accès réservé aux étudiants'], 403);
+            }
+
+            $data = json_decode($request->getContent(), true);
+            $experience = $experienceService->manageExperience($user, $data);
+
+            if (is_array($experience) && isset($experience['error'])) {
+                return new JsonResponse(['error' => $experience['error']], 400);
+            }
+
+            return $this->json(
+                $experienceService->formatExperienceJson($experience),
+                200
+            );
+        }catch (\Exception $e){
+            return new JsonResponse(['error' => $e->getMessage()], 500);
+        }
+    }
+
+    #[Route('/student/experience/{id}', name: 'app_get_experience', methods: ['GET'])]
+    public function getExperience(int $id, Request $request, ExperienceService $experienceService, TokenService $tokenService): JsonResponse
+    {
+        try {
+            $user = $tokenService->getUserFromRequest($request);
+
+            if ($user->getRole() !== 'student') {
+                return new JsonResponse(['error' => 'Accès interdit'], 403);
+            }
+
+            $result = $experienceService->getExperience($user, $id);
+
+            // si le tableau est vide : on renvoie une erreur 400 sinon 200
+            if (is_array($result) && isset($result['error'])) {
+                return new JsonResponse($result, 400);
+            }
+
+            return new JsonResponse($result, 200);
+        } catch (\Exception $e) {
+            return new JsonResponse(['error' => $e->getMessage()], 500);
+        }
+    }
+
+    #[Route('/student/experiences', name: 'app_get_all_experiences', methods: ['GET'])]
+    public function getAllExperiences(Request $request, ExperienceService $experienceService, TokenService $tokenService): JsonResponse
+    {
+        try {
+            $user = $tokenService->getUserFromRequest($request);
+
+            if ($user->getRole() !== 'student') {
+                return new JsonResponse(['error' => 'Accès interdit'], 403);
+            }
+
+            $result = $experienceService->getAllExperiences($user);
+
+            if (is_array($result) && isset($result['error'])) {
+                return new JsonResponse($result, 400);
+            }
+
+            return new JsonResponse($result, 200);
+        } catch (\Exception $e) {
+            return new JsonResponse(['error' => $e->getMessage()], 500);
+        }
+    }
+
+
+    #[Route('/student/delete-experience/{id}', name: 'app_delete_experience', methods: ['DELETE'])]
+    public function deleteExperience(int $id, Request $request, ExperienceService $experienceService, TokenService $tokenService): JsonResponse
+    {
+        try {
+            $user = $tokenService->getUserFromRequest($request);
+
+            if ($user->getRole() !== 'student') {
+                return new JsonResponse(['error' => 'Accès interdit'], 403);
+            }
+
+            $result = $experienceService->deleteExperience($user, $id);
+
+            $status = isset($result['error']) ? 400 : 200;
+            return new JsonResponse($result, $status);
+        } catch (\Exception $e) {
+            return new JsonResponse(['error' => $e->getMessage()], 500);
+        }
+    }
+
+}
--- back/src/Service/ExperienceService.php
+++ back/src/Service/ExperienceService.php
@@ -6,24 +6,120 @@ use App\Entity\Users;
 use App\Entity\Student;
 use App\Entity\Experience;
 use App\Repository\ExperienceRepository;
+use App\Repository\StudentRepository;
 use Doctrine\ORM\EntityManagerInterface;
 
 class ExperienceService
 {
     private $experienceRepository;
+    private $studentRepository;
     private $em;
 
-    public function __construct(ExperienceRepository $experienceRepository, EntityManagerInterface $em)
+    public function __construct(ExperienceRepository $experienceRepository, EntityManagerInterface $em, StudentRepository $studentRepository)
     {
         $this->experienceRepository = $experienceRepository;
+        $this->studentRepository = $studentRepository;
         $this->em = $em;
     }
 
-    public function manageExperience(Users $user, array $data, ?int $id = null): Experience
+    public function manageExperience(Users $user, array $data): Experience|array
     {
-        // Commencer par vérifier si l'utilisateur et l'expérience existent
-        // Sinon renvoyer une message d'erreur en json
-        // SI oui ajouter la nouvelle expérience
-        // Si existe déjà, mettre à jour dans la base de données
+        $student = $this->studentRepository->findOneBy(['user' => $user]);
+        if(!$student){
+            return ['error' => 'L\'étudiant n\'existe pas !'];
+        }
+
+        if (!empty($data['id'])) {
+            $experience = $this->experienceRepository->find($data['id']);
+    
+            if (!$experience) {
+                return ['error' => 'Expérience non trouvée pour mise à jour'];
+            }
+
+            if ($experience->getStudent() !== $student) {
+                return ['error' => 'Accès interdit à cette expérience'];
+            }
+        } else {
+            $experience = new Experience();
+            $experience->setStudent($student);
+        }
+
+        $experience->setCompanyName($data['companyName'] ?? '');
+        $experience->setJobTitle($data['jobTitle'] ?? '');
+        $experience->setDescription($data['description'] ?? '');
+        $experience->setStartDate(new \DateTime($data['startDate']));
+        $experience->setEndDate(new \DateTime($data['endDate']));
+
+        $this->em->persist($experience);
+        $this->em->flush();
+
+        return $experience;
     }
+
+    public function deleteExperience(Users $user, int $id): array
+    {
+        $student = $this->studentRepository->findOneBy(['user' => $user]);
+        if (!$student) {
+            return ['error' => 'Étudiant introuvable'];
+        }
+
+        $experience = $this->experienceRepository->find($id);
+        if (!$experience) {
+            return ['error' => 'Expérience introuvable'];
+        }
+
+        if ($experience->getStudent() !== $student) {
+            return ['error' => 'Accès refusé à cette expérience'];
+        }
+
+        $this->em->remove($experience);
+        $this->em->flush();
+
+        return ['message' => 'Expérience supprimée avec succès'];
+    }
+
+    public function getExperience(Users $user, int $id): array
+    {
+        $student = $this->studentRepository->findOneBy(['user' => $user]);
+        if (!$student) {
+            return ['error' => 'Étudiant introuvable'];
+        }
+    
+        $experience = $this->experienceRepository->find($id);
+        if (!$experience) {
+            return ['error' => 'Expérience introuvable'];
+        }
+    
+        if ($experience->getStudent() !== $student) {
+            return ['error' => 'Accès interdit à cette expérience'];
+        }
+
+        return $this->modelJson($experience);
+    }
+
+    public function getAllExperiences(Users $user): array
+    {
+        $student = $this->studentRepository->findOneBy(['user' => $user]);
+        if (!$student) {
+            return ['error' => 'Étudiant introuvable'];
+        }
+
+        $experiences = $this->experienceRepository->findBy(['student' => $student]);
+
+        return array_map([$this, 'modelJson'], $experiences);
+    }
+
+    public function modelJson(Experience $experience): array
+    {
+        return [
+            'id' => $experience->getId(),
+            'companyName' => $experience->getCompanyName(),
+            'jobTitle' => $experience->getJobTitle(),
+            'description' => $experience->getDescription(),
+            'startDate' => $experience->getStartDate()?->format('Y-m-d'),
+            'endDate' => $experience->getEndDate()?->format('Y-m-d'),
+        ];
+    }
+
+
 }
