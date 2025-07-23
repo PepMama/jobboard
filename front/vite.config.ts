12e5b5ee4bdc0a85e075fcffaadeecd606a9ac2f
Maelic Pepin <pmaelic@outlook.fr>
2025-07-23 19:26:32 +0200

    PM - :sparkles: Mise à jour de render.yaml pour intégrer la nouvelle structure de site statique et ajustement des routes. Modification de vite.config.ts pour changer le chemin de base en './'.

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,20 +1,18 @@
 import { fileURLToPath, URL } from 'node:url'
+import { defineConfig }      from 'vite'
+import vue                  from '@vitejs/plugin-vue'
+import vueJsx               from '@vitejs/plugin-vue-jsx'
+import vueDevTools          from 'vite-plugin-vue-devtools'
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
-
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: './',
   plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-server: {
+  server: {
     proxy: {
       '/uploads': {
         target: process.env.VITE_API_URL || 'http://localhost:8000',
@@ -23,6 +21,4 @@ server: {
       },
     },
   },
-
 })
-
